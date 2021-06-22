c51b99e552b0d6028cf523c2d9b0f0f543510610
Najir Seikh <56752564+NajirSeikh@users.noreply.github.com>
2021-06-22 22:03:47 +0530

    Add files via upload

---
+++ day1/script.js
@@ -0,0 +1,15 @@
+// Coded by Najir Seikh
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
