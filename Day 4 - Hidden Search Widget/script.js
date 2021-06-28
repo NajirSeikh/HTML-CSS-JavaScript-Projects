6f5e54cab8dab3b804ee9ae8f66ebd053495f463
NajirSeikh <ns.soft.97@gmail.com>
2021-06-28 14:37:40 +0530

    Complete Day 4 - Hidden Search Widget

---
+++ Day 4 - Hidden Search Widget/script.js
@@ -0,0 +1,9 @@
+// Coded by Najir Seikh
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
